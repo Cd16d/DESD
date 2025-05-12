c99622188dd7a63af45885190fd7f4711f958a1e
Cd16d <davidecavagnola@gmail.com>
2025-05-12 14:38:11 +0200

    Update design files for LAB3: reorganize components and adjust simulation settings

--- LAB2/design/lab_2/hdl/lab_2_wrapper.vhd
+++ LAB2/design/lab_2/hdl/lab_2_wrapper.vhd
@@ -1,8 +1,8 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
---Date        : Fri Apr 25 22:08:38 2025
---Host        : DavideASUS running 64-bit major release  (build 9200)
+--Date        : Mon May 12 14:33:04 2025
+--Host        : Davide-Samsung running 64-bit major release  (build 9200)
 --Command     : generate_target lab_2_wrapper.bd
 --Design      : lab_2_wrapper
 --Purpose     : IP block netlist
